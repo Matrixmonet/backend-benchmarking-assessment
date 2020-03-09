a68f3ba87948de43151603fccc303fd96b4684bb
Matrixmonet <successismyne@gmail.com>
2020-03-08 20:21:10 -0400

    Initial Commit

--- anagrams.py
+++ anagrams.py
@@ -3,12 +3,10 @@
 """ anagrams
     Command line interface that accepts a word file and returns a dictionary of
     anagrams for that file.
-
     Module provides a function find_anagrams which can be used to do the same
     for an arbitrary list of strings.
-
 """
-__author__ = "???"
+__author__ = "Sarah Gale"
 
 import sys
 
@@ -17,42 +15,38 @@ def alphabetize(string):
     """ alphabetize
         Given a string, return a string that includes the same letters in
         alphabetical order.
-
         Example:
-
         >>> print alphabetize('cab')
         abc
-
     """
     return "".join(sorted(string.lower()))
 
 
 def find_anagrams(words):
     """ find_anagrams
-
         Return a dictionary with keys that are alphabetized words and values
         that are all words that, when alphabetized, match the key.
-
         Example:
-
         >>> print find_anagrams(['cat', 'dog', 'act'])
         {'dgo': ['dog'], 'act': ['cat', 'act']}
-
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
-    return anagrams
+
+    anagram_sets = {}
+    for word in words:
+        if alphabetize(word) in anagram_sets:
+            anagram_sets[alphabetize(word)].append(word)
+        else:
+            anagram_sets[alphabetize(word)] = [word]
+
+    return anagram_sets
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            print(find_anagrams(words))
